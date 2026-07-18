6f0d60b48fd49f6fea20ed3ebaf3e16e7af14747
Diego <diego.fraylo@hotmail.com>
2026-07-18 12:38:53 -0600

    (v.0.3) Added more folders and more keys
    Added the function to read the folders with whatever .wav file is there and choose randomly a sound on each press, also added functions for different specific keys.

--- src/sound_manager.py
+++ src/sound_manager.py
@@ -1,14 +1,53 @@
+from pathlib import Path
 import pygame
+import random
 
 
 class SoundManager:
+
     def __init__(self):
         pygame.mixer.init()
 
-        self.click_sound = pygame.mixer.Sound("sounds/key/key.wav")
+        self.key_sounds = self._load_folder("sounds/key")
+        self.space_sounds = self._load_folder("sounds/space")
+        self.enter_sounds = self._load_folder("sounds/enter")
+        self.backspace_sounds = self._load_folder("sounds/backspace")
+        self.shift_sounds = self._load_folder("sounds/shift")
+
+    def _load_folder(self, folder):
+        try:
+            return [
+                pygame.mixer.Sound(str(file)) for file in Path(folder).glob("*.wav")
+            ]
+        except Exception as e:
+            print("Ocurrió un error:", e)
 
     def play_click(self):
         try:
-            self.click_sound.play()
+            random.choice(self.key_sounds).play()
         except Exception as e:
-            pass
+            print(e)
+
+    def play_space(self):
+        try:
+            random.choice(self.space_sounds).play()
+        except Exception as e:
+            print(e)
+
+    def play_enter(self):
+        try:
+            random.choice(self.enter_sounds).play()
+        except Exception as e:
+            print(e)
+
+    def play_backspace(self):
+        try:
+            random.choice(self.backspace_sounds).play()
+        except Exception as e:
+            print(e)
+
+    def play_shift(self):
+        try:
+            random.choice(self.shift_sounds).play()
+        except Exception as e:
+            print(e)
